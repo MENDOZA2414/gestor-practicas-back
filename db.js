61c55208a1f58bebf58704f2e3c096679a674a82
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2024-06-12 02:03:16 -0700

    Prueba 1

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const MYSQLHOST = process.env.MYSQLHOST || 'localhost';
+const MYSQLUSER = process.env.MYSQLUSER || 'root';
+const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'Jm241410';
+const MYSQLDATABASE = process.env.MYSQLDATABASE || 'sistemaPracticas';
+const MYSQLPORT = process.env.MYSQLPORT || 3306;
+
+const pool = mysql.createPool({
+    host: MYSQLHOST,
+    user: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASE,
+    port: MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10, 
+    queueLimit: 0,
+    connectTimeout: 100000,
+});
+
+module.exports = pool.promise();
